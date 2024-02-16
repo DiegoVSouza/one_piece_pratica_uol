38a341a53e5fed8a8bae2c7630edc0bf48138d30
Diego Vieira <dvsdiegovieira@gmail.com>
2024-02-16 16:43:40 -0300

    feat: bounty informations

---
+++ bountys.js
@@ -0,0 +1,231 @@
+export const bountysArray = [
+    {
+        id: 0,
+        title: "Luffy",
+        description: `
+RELATÓRIO CONFIDENCIAL DA MARINHA - ALTO RISCO<br/>
+
+Nome do Alvo: Monkey D. Luffy<br/>
+Codinome Pirata: "Chapéu de Palha"<br/>
+<br/>
+Descrição Física:<br/>
+Monkey D. Luffy é um indivíduo do sexo masculino, com idade aproximada de 19 anos. Possui cabelos pretos, olhos expressivos e um físico atlético. Notavelmente, ele ostenta um característico chapéu de palha, daí seu codinome pirata.<br/>
+<br/>
+Recompensa Atual:<br/>
+Bounty atual estimado em 1.500.000.000 de Berries. (Nota: Valor em constante atualização)<br/>
+<br/>
+Akuma no Mi - Fruto do Diabo:<br/>
+Luffy é um consumidor da Fruta Gomu Gomu, conferindo-lhe a habilidade de esticar seu corpo como se fosse feito de borracha. Essa peculiaridade o torna um oponente imprevisível e altamente perigoso em combate.<br/>
+<br/>
+Histórico Criminal:<br/>
+Luffy é o capitão dos Piratas do Chapéu de Palha e é responsável por inúmeras violações das leis marítimas. Ele é acusado de ataques a instalações governamentais, confrontos com agentes da Marinha e sabotagem de operações oficiais. Sua lista de crimes inclui ainda a libertação de prisioneiros perigosos e a desestruturação de estabelecimentos prisionais.<br/>
+<br/>
+Aliados Conhecidos:<br/>
+Luffy lidera uma tripulação conhecida como os "Chapéus de Palha", composta por membros altamente habilidosos, cada um com sua própria especialidade. Alguns membros notáveis incluem Roronoa Zoro, Nami, Usopp, Sanji, Tony Tony Chopper, Nico Robin, Franky, Brook e Jinbe.<br/>
+<br/>
+Ameaça Potencial:<br/>
+Luffy representa uma ameaça significativa à estabilidade marítima. Sua habilidade em desafiar e derrotar adversários poderosos, incluindo Shichibukai, Yonko e Almirantes, coloca-o entre os piratas mais formidáveis atualmente ativos.<br/>
+<br/>
+Modus Operandi:<br/>
+O alvo demonstra uma natureza imprudente e impulsiva, muitas vezes desafiando autoridades e confrontando diretamente forças da Marinha. Ele é conhecido por sua determinação inabalável em busca do "One Piece" e pelo desejo de liberdade para si e seus companheiros.<br/>
+<br/>
+Recomendações:<br/>
+A captura de Monkey D. Luffy é considerada de alta prioridade. Agentes destacados para operações contra o alvo devem estar preparados para enfrentar resistência intensa. A cooperação entre as unidades da Marinha é essencial para garantir o sucesso dessas operações.<br/>
+`
+    },
+    {
+        id: 1,
+        title: "Zoro",
+        description: `
+RELATÓRIO CONFIDENCIAL DA MARINHA - ALTO RISCO<br/>
+
+Nome do Alvo: Roronoa Zoro<br/>
+Codinome Pirata: "Pirata Caçador de Espadas"<br/>
+<br/>
+Descrição Física:<br/>
+Roronoa Zoro é um espadachim habilidoso com idade aproximada de 21 anos. Ele possui cabelos verdes, músculos bem desenvolvidos e três cicatrizes distintas no olho esquerdo. Seu estilo de luta com três espadas o torna uma ameaça formidável.<br/>
+<br/>
+Recompensa Atual:<br/>
+Bounty atual estimado em 320.000.000 de Berries. (Nota: Valor em constante atualização)<br/>
+<br/>
+Histórico Criminal:<br/>
+Zoro é conhecido por envolvimento em duelos mortais, desafiando outros espadachins e causando tumultos em portos. Sua perícia com a espada o torna um adversário perigoso.<br/>
+<br/>
+Aliados Conhecidos:<br/>
+Zoro faz parte dos "Chapéus de Palha" e é o espadachim leal da tripulação. Ele é conhecido por sua lealdade e dedicação à busca do "One Piece".<br/>
+<br/>
+Ameaça Potencial:<br/>
+Zoro representa uma ameaça significativa, especialmente para outros espadachins. Sua habilidade em usar três espadas simultaneamente o coloca entre os melhores espadachins do mundo.<br/>
+<br/>
+Modus Operandi:<br/>
+O alvo geralmente é encontrado treinando intensivamente ou desafiando outros espadachins em duelos. Ele é conhecido por sua ética rigorosa em relação à espada e sua determinação em se tornar o maior espadachim.<br/>
+<br/>
+Recomendações:<br/>
+Embora Zoro seja um aliado dos Chapéus de Palha, sua habilidade com a espada e seu histórico requerem vigilância contínua da Marinha.<br/>
+`
+    },
+    {
+        id: 2,
+        title: "Nami",
+        description: `
+RELATÓRIO CONFIDENCIAL DA MARINHA - ALTO RISCO<br/>
+
+Nome do Alvo: Nami<br/>
+Codinome Pirata: "Navegadora"<br/>
+<br/>
+Descrição Física:<br/>
+Nami é uma mulher jovem, com cabelos ruivos e olhos expressivos. Ela é conhecida por sua habilidade em navegação e cartografia, essenciais para as viagens dos Piratas do Chapéu de Palha.<br/>
+<br/>
+Recompensa Atual:<br/>
+Bounty atual estimado em 66.000.000 de Berries. (Nota: Valor em constante atualização)<br/>
+<br/>
+Histórico Criminal:<br/>
+Nami foi anteriormente uma ladra de mapas, mas desde então se juntou aos Chapéus de Palha. Ela é conhecida por desafiar o governo mundial em busca de liberdade para sua ilha natal.<br/>
+<br/>
+Aliados Conhecidos:<br/>
+Nami é a navegadora da tripulação e possui habilidades cruciais para a navegação e sobrevivência. Ela é leal aos Chapéus de Palha e desempenha um papel vital nas aventuras da tripulação.<br/>
+<br/>
+Ameaça Potencial:<br/>
+Embora Nami não seja uma combatente física, sua habilidade em manipular o clima usando o Climatact a torna uma ameaça em combate estratégico.<br/>
+<br/>
+Modus Operandi:<br/>
+Nami é frequentemente encontrada mapeando rotas para a tripulação e fazendo previsões meteorológicas. Ela é conhecida por sua inteligência e astúcia em situações difíceis.<br/>
+<br/>
+Recomendações:<br/>
+Apesar de sua aparência inofensiva, Nami é uma peça-chave nos Chapéus de Palha. Sua detenção pode causar sérios problemas para a tripulação.<br/>
+`
+    },
+    {
+        id: 3,
+        title: "Usopp",
+        description: `
+RELATÓRIO CONFIDENCIAL DA MARINHA - ALTO RISCO<br/>
+
+Nome do Alvo: Usopp<br/>
+Codinome Pirata: "Atirador da Tripulação"<br/>
+<br/>
+Descrição Física:<br/>
+Usopp é um jovem atirador com idade aproximada de 19 anos. Possui cabelos verdes, um nariz proeminente e é frequentemente visto com óculos de proteção. Sua imaginação vívida e habilidades de atirador o tornam um membro valioso dos Chapéus de Palha.<br/>
+<br/>
+Recompensa Atual:<br/>
+Bounty atual estimado em 200.000.000 de Berries. (Nota: Valor em constante atualização)<br/>
+<br/>
+Histórico Criminal:<br/>
+Usopp é conhecido por suas exageradas histórias e embustes, mas ele se redimiu como um membro essencial da tripulação. Ele não possui crimes significativos, mas sua habilidade em enganar oponentes é notável.<br/>
+<br/>
+Aliados Conhecidos:<br/>
+Usopp é o atirador e arqueiro dos Chapéus de Palha. Ele é próximo de Luffy desde a infância e desempenha um papel vital nas estratégias da tripulação.<br/>
+<br/>
+Ameaça Potencial:<br/>
+A habilidade de Usopp em criar e usar dispositivos engenhosos o torna uma ameaça à distância. Seu precisão como atirador é temida por inimigos.<br/>
+<br/>
+Modus Operandi:<br/>
+Usopp é frequentemente visto criando e aprimorando suas invenções. Ele usa suas habilidades para fornecer apoio à tripulação durante batalhas e situações desafiadoras.<br/>
+<br/>
+Recomendações:<br/>
+Embora seja propenso a exageros, Usopp é um membro valioso dos Chapéus de Palha. A subestimação de suas habilidades pode levar a sérias consequências.<br/>
+`
+    },
+    {
+        id: 4,
+        title: "Robin",
+        description: `
+RELATÓRIO CONFIDENCIAL DA MARINHA - ALTO RISCO<br/>
+
+Nome do Alvo: Nico Robin<br/>
+Codinome Pirata: "Historiadora do Bando"<br/>
+<br/>
+Descrição Física:<br/>
+Nico Robin é uma mulher jovem com idade aproximada de 30 anos. Possui longos cabelos negros e uma presença serena. Sua tatuagem nas costas representa a capacidade de florescer várias mãos, uma habilidade única adquirida através da Akuma no Mi.<br/>
+<br/>
+Recompensa Atual:<br/>
+Bounty atual estimado em 130.000.000 de Berries. (Nota: Valor em constante atualização)<br/>
+<br/>
+Akuma no Mi - Fruto do Diabo:<br/>
+Robin consumiu a Fruta Hana Hana no Mi, concedendo-lhe a capacidade de fazer florescer partes do corpo em qualquer superfície. Essa habilidade a torna uma pesquisadora e combatente formidável.<br/>
+<br/>
+Histórico Criminal:<br/>
+Robin já foi considerada uma criminosa, mas agora é uma valiosa aliada dos Chapéus de Palha. Ela é acusada de posse de conhecimento proibido e envolvimento com a organização criminosa Baroque Works.<br/>
+<br/>
+Aliados Conhecidos:<br/>
+Robin é a arqueóloga dos Chapéus de Palha e possui um vasto conhecimento sobre a história do mundo. Ela é respeitada por sua inteligência e habilidades em combate.<br/>
+<br/>
+Ameaça Potencial:<br/>
+A habilidade de Robin em desvendar segredos antigos a torna uma ameaça aos governantes do mundo. Ela é conhecida por sua habilidade de incapacitar inimigos usando seu poder da Hana Hana no Mi.<br/>
+<br/>
+Modus Operandi:<br/>
+Robin geralmente é vista lendo e pesquisando informações antigas. Ela contribui significativamente para a compreensão dos Poneglyphs e a busca do "One Piece".<br/>
+<br/>
+Recomendações:<br/>
+A vigilância contínua de Robin é necessária devido ao seu conhecimento crítico sobre a história mundial. Sua captura pode ter consequências significativas.<br/>
+`
+    },
+    {
+        id: 5,
+        title: "Chopper",
+        description: `
+RELATÓRIO CONFIDENCIAL DA MARINHA - ALTO RISCO<br/>
+
+Nome do Alvo: Tony Tony Chopper<br/>
+Codinome Pirata: "Médico da Tripulação"<br/>
+<br/>
+Descrição Física:<br/>
+Tony Tony Chopper é uma rena com idade aproximada de 17 anos. Ele possui a habilidade da Akuma no Mi de se transformar em várias formas, incluindo uma forma humanoide. Sua personalidade carinhosa e habilidades médicas o tornam um membro valioso dos Chapéus de Palha.<br/>
+<br/>
+Recompensa Atual:<br/>
+Bounty atual estimado em 100.000.000 de Berries. (Nota: Valor em constante atualização)<br/>
+<br/>
+Akuma no Mi - Fruto do Diabo:<br/>
+Chopper consumiu a Fruta Hito Hito no Mi, Modelo: Daibutsu, concedendo-lhe a capacidade de se transformar em diversas formas, incluindo uma forma humana. Sua forma humana é conhecida como "Monster Point".<br/>
+<br/>
+Histórico Criminal:<br/>
+Chopper não possui histórico criminal significativo. Ele é conhecido por sua dedicação à medicina e por ajudar a tripulação em situações médicas de emergência.<br/>
+<br/>
+Aliados Conhecidos:<br/>
+Chopper é o médico dos Chapéus de Palha e oferece cuidados médicos essenciais para a tripulação. Sua habilidade de se transformar em formas diferentes é útil tanto em combate quanto em situações médicas.<br/>
+<br/>
+Ameaça Potencial:<br/>
+Embora seja um combatente capaz em sua forma "Monster Point", Chopper geralmente evita o combate direto. Ele é mais eficaz no papel de médico e suporte.<br/>
+<br/>
+Modus Operandi:<br/>
+Chopper é frequentemente visto cuidando da saúde da tripulação e oferecendo conselhos médicos. Sua natureza gentil e carinhosa o torna querido pelos Chapéus de Palha.<br/>
+<br/>
+Recomendações:<br/>
+Chopper deve ser considerado como um recurso valioso para assistência médica. Seu conhecimento em medicina é vital para a saúde e bem-estar da tripulação.<br/>
+`
+    },
+    {
+        id: 6,
+        title: "Brook",
+        description: `
+RELATÓRIO CONFIDENCIAL DA MARINHA - ALTO RISCO<br/>
+
+Nome do Alvo: Brook<br/>
+Codinome Pirata: "Músico Esquelético"<br/>
+<br/>
+Descrição Física:<br/>
+Brook é um esqueleto vivo com idade aproximada de 90 anos. Ele possui uma cartola, um violão e uma cicatriz no olho direito. Sua habilidade de retornar à vida após consumir a Fruta Yomi Yomi no Mi o torna uma presença única e valiosa nos Chapéus de Palha.<br/>
+<br/>
+Recompensa Atual:<br/>
+Bounty atual estimado em 83.000.000 de Berries. (Nota: Valor em constante atualização)<br/>
+<br/>
+Akuma no Mi - Fruto do Diabo:<br/>
+Brook consumiu a Fruta Yomi Yomi no Mi, conferindo-lhe a capacidade de retornar à vida após a morte. Isso o torna um "esqueleto vivo" e lhe permite usar os poderes da Akuma no Mi.<br/>
+<br/>
+Histórico Criminal:<br/>
+Brook não possui histórico criminal significativo. Ele é conhecido por sua habilidade musical e por trazer alegria à tripulação com sua música.<br/>
+<br/>
+Aliados Conhecidos:<br/>
+Brook é o músico dos Chapéus de Palha e contribui para o moral da tripulação com suas habilidades musicais. Ele também possui habilidades em esgrima.<br/>
+<br/>
+Ameaça Potencial:<br/>
+A habilidade de Brook de retornar à vida o torna resistente a muitas formas de dano. Ele também é um espadachim habilidoso e pode ser uma ameaça em combate.<br/>
+<br/>
+Modus Operandi:<br/>
+Brook é frequentemente visto tocando música para a tripulação e participando de situações cômicas. Sua natureza alegre e otimista o torna um membro querido.<br/>
+<br/>
+Recomendações:<br/>
+Apesar de sua aparência incomum, Brook é um membro valioso e pode oferecer suporte tanto em combate quanto com suas habilidades musicais.<br/>
+`
+    },
+]
